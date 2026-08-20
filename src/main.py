8ae9751979d7b8c3f9448c981d129b06e4425fc1
Hassan Abdi Farah <farahabdihassan2@gmail.com>
2026-08-21 00:24:25 +0300

    Add Python functions in utils.py and execution logic in main.py

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from utils import square, is_even, celsius_to_fahrenheit
+
+def process_number(n):
+    print(f"--- Results for {n} ---")
+    print(f"Square: {square(n)}")
+    print(f"Is Even: {is_even(n)}")
+    print(f"As Celsius to Fahrenheit: {celsius_to_fahrenheit(n):.2f}°F\n")
+
+if __name__ == "__main__":
+    for _ in range(3):
+        val = float(input("Enter a number: "))
+        process_number(val)
