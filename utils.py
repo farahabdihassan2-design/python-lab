2a9f55be93e41405ba3d8d108d385bc15ea4dbff
Hassan Abdi Farah <farahabdihassan2@gmail.com>
2026-08-21 19:27:37 +0300

    Add utility functions for math and greetings

--- utils.py
+++ utils.py
@@ -1,2 +1,11 @@
-def helper():
-    pass
+def square(n):
+    return n ** 2
+
+def is_even(n):
+    return n % 2 == 0
+
+def celsius_to_fahrenheit(c):
+    return (c * 9/5) + 32
+
+def greet(name):
+    return f"Hello, {name}!" 
